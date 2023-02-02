a61239319cb8117ff0769a9515feb990b24882ff
Tim White <tim94white@gmail.com>
2023-02-02 14:59:14 +0000

    initial commit

---
+++ CosmosRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace TodoList
+{
+    public class CosmosRepository
+    {
+        Database todoDatabase;
+        Container todoContainer;
+        public CosmosRepository()
+        {
+            CosmosClient client = new CosmosClient("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+            todoDatabase = client.GetDatabase("TodoApp");
+            todoContainer = todoDatabase.GetContainer("Todo");
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetAll()
+        {
+            var response = todoContainer.GetItemLinqQueryable<TodoItem>(true).ToList();
+            return response;
+
+        }
+
+        public async Task Add(TodoItem item)
+        {
+            await todoContainer.CreateItemAsync(item);
+        }
+
+    }
+}
---
+++ TodoItem.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using TodoList.Pages;
+
+public class TodoContext : DbContext
+{
+    public DbSet<TodoItem> Todos { get; set; }
+    public string? Dbpath { get; }
+    public TodoContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        Dbpath = System.IO.Path.Join(path, "todo.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={Dbpath}");
+}
+public class TodoItem
+{
+    [JsonProperty("id")]
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+}
